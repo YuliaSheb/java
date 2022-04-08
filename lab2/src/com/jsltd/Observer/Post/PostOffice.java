11b72e8fc350b33e96a4be9bab1cdf7b192e11ce
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2022-04-08 14:52:49 +0300

    Delete lab2 directory

--- lab2/src/com/jsltd/Observer/Post/PostOffice.java
+++
@@ -1,9 +0,0 @@
-package com.jsltd.Observer.Post;
-
-import com.jsltd.Observer.Subscriber.People;
-
-public interface PostOffice {
-    void registerPeople(People peoples);
-    void removePeople(People peoples);
-    void notifyPeople();
-}
--- lab2/src/com/jsltd/Observer/PublishingHouse/PublishingHouse.java
+++
@@ -1,6 +0,0 @@
-package com.jsltd.Observer.PublishingHouse;
-
-public interface PublishingHouse {
-    void handleEvent();
-    void notifyPost();
-}
